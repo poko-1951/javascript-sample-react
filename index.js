094002eeb13d2e6fc4f3d5db6b27de7d595ad390
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-08-25 15:20:33 +0900

    [Add]HTML&CSSを整えた

---
+++ index.js
@@ -0,0 +1,57 @@
+// const name = "aaa";
+// const message1 = "私の名前は${name}";
+
+// console.log(message1);
+
+
+// // function func(str) {
+// //   return str; 
+// // }
+// // console.log(func("aefae"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+
+// console.log(func2("fund2です"));
+
+// // 分割代入
+// const my = {
+//    name: "な",
+//    age: 27,
+// };
+
+
+// const arr = [1, 2, 3];
+
+// const sumfunc = (num1, num2) => console.log(num1 + num2);
+
+// // sumfunc(arr[0], arr[1]);
+// sumfunc(...arr)
+
+// const arr = [1,2];
+// const arr4 = arr;
+// // console.log(arr4);
+// const arr5 = [...arr]
+// console.log(arr5)
+
+// const nameArr = ["田中", "鈴木", "佐藤"];
+// for (let index = 0; index < nameArr.length; index++){
+//   console.log(nameArr[index])
+// };
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// });
+// console.log(nameArr2)
+
+// const Arr = [1,2,3,4,5];
+// const newArr = Arr.filter((num)=>{
+//   return num % 2 === 0;
+// });
+// console.log(newArr);
+
+const num = 2;
+const fee = "あああ" && num;
+console.log(fee);
